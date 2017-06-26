f67944f1e9106a5d6b86e8f62d958167bbb1ef1c
潘安 <wslongchen@vip.qq.com>
2017-06-26 15:49:12 +0800

    add command
    add command

---
+++ bin/webwx.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node  
+var wxbot = require('../wxbot')  
+var program = require('commander');  
+  
+program.version('v' + require('../package.json').version)  
+       .description('easy to user wechat');
+
+program.command('test')  
+       .alias('example')  
+       .description('启动一个案例')  
+       .action(function () {  
+        	wxbot.start();
+       });
+program.parse(process.argv)  
+  
+if (program.args.length === 0) {  
+  program.help()  
+}  
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 // 导入基本模块
-var mailer = require("./libs/core/mail");
-var wechatapi = require('./wechatapi');
-var config = require('./libs/core/config');
+var mailer = require("../libs/core/mail");
+var wechatapi = require('./src/wechatapi');
+var config = require('../libs/core/config');
 var schedule = require('node-schedule');
 
 //联系人列表
