6d98bf664eabb795f1a200a8591aff132ef804c6
潘安 <wslongchen@vip.qq.com>
2017-06-29 14:35:15 +0800

    make npm version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 ﻿#!/usr/bin/env node
 module.exports = require('./src/wechatapi');
 module.exports = require('./libs/core/config');
-module.exports = require('./wxbot');
+module.exports = require('./src/wxapi');
